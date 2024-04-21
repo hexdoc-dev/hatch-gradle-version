61917f872b49e56dcf5812ed257edb3d86e4b3fe
object-Object <object@objectobject.ca>
2024-04-21 15:15:26 -0400

    Partially support non-string versions in version catalog

--- src/hatch_gradle_version/plugins/metadata_hook/test_version_catalog.py
+++ src/hatch_gradle_version/plugins/metadata_hook/test_version_catalog.py
@@ -2,6 +2,7 @@ import copy
 from pathlib import Path
 from textwrap import dedent
 
+import pytest
 from pytest import MonkeyPatch
 
 from .version_catalog import VersionCatalogMetadataHook
@@ -15,9 +16,10 @@ def test_gradle_properties_deps(tmp_path: Path, monkeypatch: MonkeyPatch):
     version_catalog.parent.mkdir(parents=True)
     version_catalog.write_text(
         dedent(
-            f"""\
+            """\
             [versions]
             K = "1.2.3"
+            other = { strictly="1.0" }
             """
         )
     )
@@ -53,3 +55,45 @@ def test_gradle_properties_deps(tmp_path: Path, monkeypatch: MonkeyPatch):
         "dependencies": ["P~=1.2.3.4.5"],
         "optional-dependencies": {},
     }
+
+
+def test_non_str_version_fails(tmp_path: Path, monkeypatch: MonkeyPatch):
+    # arrange
+    monkeypatch.setenv("HATCH_GRADLE_DIR", "gradle_dir")
+
+    version_catalog = tmp_path / "gradle_dir" / "gradle" / "my-libs.versions.toml"
+    version_catalog.parent.mkdir(parents=True)
+    version_catalog.write_text(
+        dedent(
+            """\
+            [versions]
+            K = { strictly="1.0" }
+            """
+        )
+    )
+
+    hook = VersionCatalogMetadataHook(
+        tmp_path.as_posix(),
+        {
+            "path": "gradle/my-libs.versions.toml",
+            "dependencies": [
+                {
+                    "package": "P",
+                    "op": "~=",
+                    "key": "K",
+                    "py-version": "4.5",
+                }
+            ],
+        },
+    )
+
+    metadata = {
+        "dynamic": [
+            "dependencies",
+            "optional-dependencies",
+        ],
+    }
+
+    # act
+    with pytest.raises(ValueError):
+        hook.update(metadata)
--- src/hatch_gradle_version/plugins/metadata_hook/version_catalog.py
+++ src/hatch_gradle_version/plugins/metadata_hook/version_catalog.py
@@ -1,7 +1,8 @@
+import tomllib
 from functools import cached_property
 from pathlib import Path
+from typing import Any
 
-import tomllib
 from pydantic import TypeAdapter
 
 from hatch_gradle_version.common.gradle import GradleDependency, GradleVersion
@@ -20,13 +21,21 @@ class VersionCatalogMetadataHook(BaseMetadataHook):
 
     def parse_gradle_dependency(self, dependency: GradleDependency):
         raw_version = self.versions[dependency.key]
-        gradle_version = GradleVersion.from_raw(raw_version, self.versions, None)
+        if not isinstance(raw_version, str):
+            raise ValueError(
+                f"Expected str for dependency {dependency.key}, got {raw_version}"
+            )
+        gradle_version = GradleVersion.from_raw(raw_version, self.str_versions, None)
         return dependency.version_specifier(gradle_version)
 
     @cached_property
-    def versions(self) -> dict[str, str]:
+    def versions(self) -> dict[str, str | dict[str, Any]]:
         with self.path.open("rb") as f:
             version_catalog = tomllib.load(f)
 
-        ta = TypeAdapter(dict[str, str])
+        ta = TypeAdapter(dict[str, str | dict[str, Any]])
         return ta.validate_python(version_catalog["versions"])
+
+    @cached_property
+    def str_versions(self):
+        return {k: v for k, v in self.versions.items() if isinstance(v, str)}
