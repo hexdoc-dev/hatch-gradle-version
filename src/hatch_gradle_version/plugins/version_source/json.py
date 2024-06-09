2f57a091fc7cce6bcafe949c72ab3e299c185062
object-Object <object@objectobject.ca>
2024-06-09 14:12:18 -0400

    Write JSON keys/values to __gradle_version__.py

--- src/hatch_gradle_version/plugins/version_source/json.py
+++ src/hatch_gradle_version/plugins/version_source/json.py
@@ -1,7 +1,8 @@
 import json
-from typing import Annotated, Any
+from typing import Annotated, Any, overload
 
 from pydantic import Field, field_validator
+from pydantic.types import JsonValue
 
 from hatch_gradle_version.common.gradle import GradleVersion
 from hatch_gradle_version.common.model import GradlePath
@@ -17,13 +18,68 @@ class JSONVersionSource(BaseVersionSource):
 
     def get_gradle_version(self) -> GradleVersion:
         with self.json_path.open() as f:
-            data = json.load(f)
+            data: JsonValue = json.load(f)
 
         raw_version = data
         for key in self.key:
+            if not isinstance(raw_version, dict):
+                raise ValueError(f"Invalid key: {self.key} (not a dict: {raw_version})")
             raw_version = raw_version[key]
 
-        return GradleVersion.from_raw(raw_version, {}, self.fmt_raw_gradle_version)
+        raw_version = self.json_to_string(raw_version)
+        if raw_version is None:
+            raise ValueError(f"Invalid key: {self.key} (invalid value: {raw_version})")
+
+        extra_versions = dict[str, str]()
+        self.get_extra_versions(data, extra_versions, [])
+
+        return GradleVersion.from_raw(
+            raw_version,
+            extra_versions,
+            self.fmt_raw_gradle_version,
+        )
+
+    def get_extra_versions(
+        self,
+        value: JsonValue,
+        result: dict[str, str],
+        parents: list[str],
+    ):
+        match value:
+            case dict():
+                for key, child in value.items():
+                    parents.append(key)
+                    self.get_extra_versions(child, result, parents)
+                    parents.pop()
+            case list():
+                for i, child in enumerate(value):
+                    parents.append(str(i) if parents else f"_{i}")
+                    self.get_extra_versions(child, result, parents)
+                    parents.pop()
+            case _:
+                result[".".join(parents)] = self.json_to_string(value)
+
+    @overload
+    def json_to_string(self, value: str | bool | int | float | None) -> str: ...
+
+    @overload
+    def json_to_string(self, value: list[JsonValue] | dict[str, JsonValue]) -> None: ...
+
+    @overload
+    def json_to_string(self, value: JsonValue) -> str | None: ...
+
+    def json_to_string(self, value: JsonValue):
+        match value:
+            case dict() | list():
+                return None
+            case True:
+                return "true"
+            case False:
+                return "false"
+            case None:
+                return "null"
+            case _:
+                return str(value)
 
     @field_validator("key", mode="before")
     @classmethod
